ee63d10da1d4f9237b874cf74fd18eed6a27fdaf
Demian Brecht <demianbrecht@gmail.com>
2013-04-19 23:29:45 -0700

    update

--- test.js
+++ test.js
@@ -1,83 +1,128 @@
-'use strict'
+"use strict"
 
 var assert = require('assert');
-var url = require('url');
+var clone = require('clone');
 var querystring = require('querystring');
+var url = require('url');
 var http = require('http');
+
 var sanction = require('./lib/sanction');
 
-var PROVIDERS = {
-    unit: {
-        authEndpoint: 'https://example.com/o/auth',
-        tokenEndpoint: 'http://127.0.0.1:4242',
-        resourceEndpoint: 'resource',
-        clientId: 'clientId',
-        clienSecret: 'clientSecret',
-        redirectUri: 'local'
-    }
+var CLIENT_OPTS = {
+    clientId: 'client_id',
+    clientSecret: 'client_secret',
+    authEndpoint: 'http://localhost:4242/o/auth',
+    tokenEndpoint: 'http://localhost:4242/o/token',
+    resourceEndpoint: 'http://localhost:4242/api',
+    redirectUri: 'http://localhost/login'
 };
 
-var TEST_SERVER = {
-    host: '127.0.0.1',
-    port: 4242
+var ACCESS_TOKEN = {
+    access_token: 'accessToken',
+    refresh_token: 'refreshToken',
+    expires_in: 4200
 };
 
-var TEST_TOKEN = 'access_token';
-var TEST_CODE = 'code';
+var USER_DATA = {
+    first_name: 'foo',
+    last_name: 'bar',
+    email: 'foo@bar.com'
+};
 
-function TokenServer() {
-    var server = null;
-    TokenServer.prototype.start = function SERVER_start(cb) {
-        server = http.createServer(function(req, res) {
-            res.write(TEST_TOKEN);
-            res.end();
-        }).listen(TEST_SERVER.port, TEST_SERVER.host, null, cb);
-    };
+describe('client', function() {
+    describe('#ctor', function() {
+        it('should have JSON.parse assigned to parser', function() {
+            var c = new sanction.Client(CLIENT_OPTS);
+            assert.equal(c.clientId, CLIENT_OPTS.clientId);
+            assert.equal(c.clientSecret, CLIENT_OPTS.clientSecret);
+            assert.equal(JSON.stringify(c.authEndpoint), 
+                JSON.stringify(url.parse(CLIENT_OPTS.authEndpoint)));
+            assert.equal(JSON.stringify(c.tokenEndpoint), 
+                JSON.stringify(url.parse(CLIENT_OPTS.tokenEndpoint)));
+            assert.equal(JSON.stringify(c.resourceEndpoint), 
+                JSON.stringify(url.parse(CLIENT_OPTS.resourceEndpoint)));
+        });
 
-    TokenServer.prototype.close = function SERVER_stop(cb) {
-        if(server) {
-            server.close(cb)
-        }
-    };
-}
+        it('should have nulled out credentials', function() {
+            var c = new sanction.Client(CLIENT_OPTS);
+            assert.equal(c.credentials.accessToken, null);
+            assert.equal(c.credentials.refreshToken, null);
+            assert.equal(c.credentials.tokenExpires, null);
+        });
+    });
 
+    describe('#authUri', function() {
+        it('should have no optional params', function(done) {
+            var c = new sanction.Client(CLIENT_OPTS);
+            c.authUri({}, function(e, uri) {
+                assert.equal(e, null);
 
-describe('Client', function() {
-    var p = PROVIDERS.unit;
-    var client = new sanction.Client(p.clientId, p.clientSecret,
-        p.authEndpoint, p.tokenEndpoint, p.resourceEndpoint, p.redirectUri);
-    var server = new TokenServer();
-
-    describe('#auth_uri()', function() {
-        it('should == ' + p.authEndpoint, function() {
-            var testqs = {redirectUri: 'unit'};
-            client.authUri(testqs, function(e, uri) {
                 var o = url.parse(uri);
-                assert.equal(o.protocol, client.authEndpoint.protocol);
-                assert.equal(o.host, client.authEndpoint.host);
-                assert.equal(querystring.parse(o.query).redirect_uri, 
-                    testqs.redirectUri);
+                var q = querystring.decode(o.query);
+                assert.equal(q.client_id, CLIENT_OPTS.clientId);
+                assert.equal(q.redirect_uri, CLIENT_OPTS.redirectUri);
+
+                var p = url.parse(CLIENT_OPTS.redirectUri);
+                assert.equal(o.protocol, p.protocol);
+                assert.equal(o.hostname, p.hostname);
+
+                assert.equal(q.state, undefined);
+                assert.equal(q.scope, undefined);
+
+                done();
             });
         });
 
-        it('should == ' + p.authEndpoint + '?scope=foo,bar', function() {
-            client.authUri({scope: 'foo,bar', redirectUri: 'unit'}, function(e, uri) {
-                var qs = querystring.parse(url.parse(uri).query);
+        it('should have a scope and state', function() {
+            var c = new sanction.Client(CLIENT_OPTS);
+            var opts = {
+                scope: 'scope',
+                state: 'state'
+            };
+            c.authUri(opts, function(e, uri) {
+                assert.equal(e, null);
 
-                assert.equal(qs.scope, 'foo,bar');
-                assert.equal(qs.redirect_uri, 'unit');
+                var q = querystring.decode(url.parse(uri).query);
+                assert.equal(q.state, opts.state);
+                assert.equal(q.scope, opts.scope);
             });
         });
     });
 
-    describe('#requestToken()', function() {
-        it('should not be null', function(done) {
-            server.start(function() {
-                client.requestToken('foo', function(e, token) {
-                    assert.equal(e, null);
-                    assert.equal(token, TEST_TOKEN);
-                    server.close(done);
-                });
+    describe('#requestToken', function() {
+        var encode = null;
+        var client = new sanction.Client(CLIENT_OPTS);
+
+        function createServer(encoder) {
+            return http.createServer(function(req, res) {
+                res.end(encoder(ACCESS_TOKEN));
+            }).listen(client.tokenEndpoint.port, client.tokenEndpoint.hostname);
+        }
+
+        it('should get a valid token via JSON', function(done) {
+            var server = createServer(JSON.stringify);
+
+            var opts = {code: 'code'};
+            client.requestToken(opts, function(e, data) {
+                assert.notEqual(data, null);
+                server.close(done);
+            });
+        });
+
+        it('should get a valid token via querystring.parse', function(done) {
+            var server = createServer(querystring.stringify);
+
+            var opts = {
+                code: 'code', 
+                parser: function(r) {
+                    var o = querystring.parse(r);
+                    o.expires_in = parseInt(o.expires_in);
+                    return o;
+                }
+            };
+            client.requestToken(opts, function(e, data) {
+                assert.equal(JSON.stringify(data), JSON.stringify(ACCESS_TOKEN));
+                server.close(done);
             });
         });
     });
